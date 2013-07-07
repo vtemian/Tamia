151cbaa8fbd58a98ccf16d8c7e5fd595a4657931
Olivier Meunier <olivier@neokraft.net>
2013-07-07 12:14:09 +0200

    Switched to MIT license

--- octopus/version.py
+++ octopus/version.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 #
-# This file is part of Octopus released under the FreeBSD license.
+# This file is part of Octopus released under the MIT license.
 # See the LICENSE for more information.
 __version__ = '0.1.1'
