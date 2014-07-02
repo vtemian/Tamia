fcec1e4e12902890bdb6ccb09310eedf00129db5
vtemian <vladtemian@gmail.com>
2014-07-02 12:46:00 +0300

    Update first

--- travis.sh
+++ travis.sh
@@ -1,3 +1,4 @@
 sudo apt-get install -y software-properties-common python-software-properties
 sudo add-apt-repository -y ppa:presslabs/testing-ppa
+sudo apt-update
 sudo apt-get install -y libgit2-0
