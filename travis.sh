40493b7b29a0f8a3bdca38f6b3ba4f0b5e961281
vtemian <vladtemian@gmail.com>
2014-07-02 13:28:33 +0300

    Added dev variant for libgit2

--- travis.sh
+++ travis.sh
@@ -1,4 +1,4 @@
 sudo apt-get install -y software-properties-common python-software-properties
 sudo add-apt-repository -y ppa:presslabs/testing-ppa
 sudo apt-get update
-sudo apt-get install -y libgit2-0
+sudo apt-get install -y libgit2-0 libgit2-dev
